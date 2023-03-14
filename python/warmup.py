3c089368483deb47445aa8bb5dc419d89061a86f
Michael Perezous <mzperezous@gmail.com>
2023-03-14 18:27:02 -0400

    update docstring with runtime

--- python/warmup.py
+++ python/warmup.py
@@ -1,7 +1,9 @@
 from typing import List
 
 def contains_duplicates(nums: List[int]) -> bool:
-    """ Checks for """
+    """ Checks for duplicate integers in a list.
+        Runtime: O(n)
+    """
     seen = {}
     for num in nums:
         if seen.get(num, None) is not None:
