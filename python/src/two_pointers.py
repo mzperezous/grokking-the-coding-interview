6db9f40381d5515963ff85ba8b1599faf9bd3a2e
Michael Perezous <mzperezous@gmail.com>
2023-03-16 03:54:04 -0400

    remove_duplicates

--- python/src/two_pointers.py
+++ python/src/two_pointers.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Iterable, List
 
 def pair_with_target_sum(nums: List[int], target: int):
     """ Returns indices of elements that add up to target if they exist.
@@ -19,3 +19,32 @@ def pair_with_target_sum(nums: List[int], target: int):
             l += 1
 
     return [-1, -1]
+
+def remove_duplicates(nums: List[int]) -> int:
+    """ Removes the duplicates from nums in place and returns the updated length.
+        Time complexity: O(n)
+        Space complexity: O(1)
+    """
+    if len(nums) == 1:
+        return 1
+
+    curr, next_non_dup = 0, 1
+    while next_non_dup < len(nums):
+        # Find all duplicates
+        if nums[curr] == nums[next_non_dup]:
+            next_non_dup += 1
+            continue
+        
+        # Remove duplicates when differing element found
+        else:
+            for i in range(curr, next_non_dup - 1):
+                nums.pop(i)
+            curr += 1
+            next_non_dup = curr + 1
+
+    # If last element is repeating, remove dups
+    if curr < len(nums) - 1:
+        for i in range(curr + 1, next_non_dup):
+            nums.pop(i)
+
+    return len(nums)
--- python/tests/test_two_pointers.py
+++ python/tests/test_two_pointers.py
@@ -1,11 +1,17 @@
 from unittest import TestCase
+from src.two_pointers import (
+    pair_with_target_sum, 
+    remove_duplicates
+)
 
-from src.two_pointers import pair_with_target_sum
-
-def TestTwoPointers(TestCase):
+class TestTwoPointers(TestCase):
     """ DesignGurus test case checks for two pointers module
     """
 
     def test_pair_with_target_sum(self):
         self.assertEqual(pair_with_target_sum([1, 2, 3, 4, 6], 6), [1, 3])
         self.assertEqual(pair_with_target_sum([2, 5, 9, 11], 11), [0, 2])
+
+    def test_remove_duplicates(self):
+        self.assertEqual(remove_duplicates([2, 3, 3, 3, 6, 9, 9]), 4)
+        self.assertEqual(remove_duplicates([2, 2, 2, 11]), 2)
