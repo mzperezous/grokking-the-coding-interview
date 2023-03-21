9870d4113f766e9f3d4e9c468b7bb82c64211d46
Michael Perezous <mzperezous@gmail.com>
2023-03-21 11:59:17 -0400

    smallest_subarray_with_gte_sum

--- python/src/sliding_window.py
+++ python/src/sliding_window.py
@@ -16,3 +16,24 @@ def max_sum_subarray(size: int, nums: List[int]) -> int:
             window_start += 1
 
     return max_sum
+
+def smallest_subarray_with_gte_sum(s: int, nums: List[int]) -> int:
+
+    def has_subarray_with_gte_sum(size: int) -> bool:
+        window_sum, window_start = 0, 0
+        for window_end, num in enumerate(nums):
+            window_sum += num
+
+            if window_end >= size:
+                if window_sum > s:
+                    return True
+                window_sum -= nums[window_start]
+                window_start += 1
+
+        return False
+
+    for i in range(1, len(nums)):
+        if has_subarray_with_gte_sum(i):
+            return i
+
+    return 0
--- python/tests/test_sliding_window.py
+++ python/tests/test_sliding_window.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from src.sliding_window import (
-    max_sum_subarray
+    max_sum_subarray,
+    smallest_subarray_with_gte_sum
 )
 
 class TestSlidingWindow(TestCase):
@@ -8,3 +9,8 @@ class TestSlidingWindow(TestCase):
     def test_max_sum_subarray(self):
         self.assertEqual(max_sum_subarray(3, [2, 1, 5, 1, 3, 2]), 9)
         self.assertEqual(max_sum_subarray(2, [2, 3, 4, 1, 5]), 7)
+
+    def test_smallest_subarray_with_gte_sum(self):
+        self.assertEqual(smallest_subarray_with_gte_sum(7, [2, 1, 5, 2, 3, 2]), 2)
+        self.assertEqual(smallest_subarray_with_gte_sum(7, [2, 1, 5, 2, 8]), 1)
+        self.assertEqual(smallest_subarray_with_gte_sum(8, [3, 4, 1, 1, 6]), 3)
