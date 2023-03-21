514fa87d807b4a0fe89e63eab40ba4b0a8084cb7
Michael Perezous <mzperezous@gmail.com>
2023-03-21 12:04:07 -0400

    optimize smallest_subarray_with_gte_sum

--- python/src/sliding_window.py
+++ python/src/sliding_window.py
@@ -18,22 +18,20 @@ def max_sum_subarray(size: int, nums: List[int]) -> int:
     return max_sum
 
 def smallest_subarray_with_gte_sum(s: int, nums: List[int]) -> int:
+    """ Review this one """
 
-    def has_subarray_with_gte_sum(size: int) -> bool:
-        window_sum, window_start = 0, 0
-        for window_end, num in enumerate(nums):
-            window_sum += num
+    min_len = None
+    window_sum, window_start = 0, 0
+    for window_end, num in enumerate(nums):
+        window_sum += num
 
-            if window_end >= size:
-                if window_sum > s:
-                    return True
-                window_sum -= nums[window_start]
-                window_start += 1
-
-        return False
-
-    for i in range(1, len(nums)):
-        if has_subarray_with_gte_sum(i):
-            return i
-
-    return 0
+        while window_sum >= s:
+            if min_len is None:
+                min_len = window_end - window_start + 1
+            else:
+                min_len = min(min_len, window_end - window_start + 1)
+            window_sum -= nums[window_start]
+            window_start += 1
+    if min_len is None:
+        return 0
+    return min_len
