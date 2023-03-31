65c4a38ab53c433e4bcc11addabccf49056e6319
Michael Perezous <mzperezous@gmail.com>
2023-03-31 13:23:45 -0400

    adapt better to problem statement

--- python/src/tree_dfs.py
+++ python/src/tree_dfs.py
@@ -8,12 +8,14 @@ class TreeNode:
 def has_sum_path(root: TreeNode, goal: int) -> bool:
     
     def sum_dfs(node: TreeNode, curr_sum: int) -> bool:
-        if node.val + curr_sum == goal:
+        if node.val + curr_sum == goal and not node.left and not node.right:
             return True
-        if node.left and sum_dfs(node.left, node.val + curr_sum):
-            return True
-        if node.right and sum_dfs(node.right, node.val + curr_sum):
-            return True
+        
+        elif node.val + curr_sum < goal:
+            if node.left and sum_dfs(node.left, node.val + curr_sum):
+                return True
+            if node.right and sum_dfs(node.right, node.val + curr_sum):
+                return True
 
         return False
 
