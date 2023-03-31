265c9ca80c21905c4ca3a647fe344876123c0b0f
Michael Perezous <mzperezous@gmail.com>
2023-03-31 12:15:18 -0400

    zigzag_level_traversal

--- python/src/tree_bfs.py
+++ python/src/tree_bfs.py
@@ -6,7 +6,7 @@ class TreeNode:
     self.value = val
     self.left, self.right = None, None
 
-def level_order_traverse(root: TreeNode) -> List[int]:
+def level_order_traverse(root: TreeNode) -> List[List[int]]:
     
     queue = deque()
     queue.append(root)
@@ -28,3 +28,55 @@ def level_order_traverse(root: TreeNode) -> List[int]:
         result.append(level)
 
     return result
+
+def reverse_level_order_traversal(root: TreeNode) -> List[List[int]]:
+
+    queue = deque()
+    result = deque()
+    queue.append(root)
+
+    while queue:
+        level, level_size = [], len(queue)
+
+        for _ in range(level_size):
+            curr = queue.popleft()
+            level.append(curr.value)
+
+            if curr.left is not None:
+                queue.append(curr.left)
+            if curr.right is not None:
+                queue.append(curr.right)
+
+        result.appendleft(level)
+
+    return list(result)
+
+def zigzag_level_traversal(root: TreeNode) -> List[List[int]]:
+
+    queue = deque()
+    result = []
+
+    queue.append(root)
+    left_to_right = True
+
+    while queue:
+        level_size = len(queue)
+        current_level = deque()
+
+        for _ in range(level_size):
+            current_node = queue.popleft()
+
+            if left_to_right:
+                current_level.append(current_node.value)
+            else:
+                current_level.appendleft(current_node.value)
+
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+            
+        result.append(list(current_level))
+        left_to_right = not left_to_right
+
+    return list(result)
--- python/tests/test_tree_bfs.py
+++ python/tests/test_tree_bfs.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from src.tree_bfs import (
     level_order_traverse,
-    TreeNode
+    reverse_level_order_traversal,
+    TreeNode,
+    zigzag_level_traversal
 )
 
 
@@ -18,4 +20,38 @@ class TestTreeBFS(TestCase):
         levels = level_order_traverse(root)
         self.assertEqual(levels, [[12], [7, 1], [9, 10, 5]])
 
-        
+    def test_reverse_level_order_traversal(self):
+        root = TreeNode(12)
+        root.left = TreeNode(7)
+        root.right = TreeNode(1)
+        root.left.left = TreeNode(9)
+        root.right.left = TreeNode(10)
+        root.right.right = TreeNode(5)
+
+        levels = reverse_level_order_traversal(root)
+        self.assertEqual(levels, [[9, 10, 5], [7, 1], [12]])
+
+    def test_zigzag_level_traversal(self):
+        root = TreeNode(12)
+        root.left = TreeNode(7)
+        root.right = TreeNode(1)
+        root.left.left = TreeNode(9)
+        root.right.left = TreeNode(10)
+        root.right.right = TreeNode(5)
+        root.right.left.left = TreeNode(20)
+        root.right.left.right = TreeNode(17)
+
+        zigzag = zigzag_level_traversal(root)
+        self.assertEqual(zigzag, [[12], [1, 7], [9, 10, 5], [17, 20]])
+
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.left.left = TreeNode(4)
+        root.left.right = TreeNode(5)
+        root.right.left = TreeNode(6)
+        root.right.right = TreeNode(7)
+
+        zigzag = zigzag_level_traversal(root)
+        self.assertEqual(zigzag, [[1], [3, 2], [4, 5, 6, 7]])
+    
